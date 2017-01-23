8ee879cf2cccc04393b2f72a0c8d28708a159f05
NathanLacey <nathn.lacey@hotmail.com>
2017-01-22 17:01:42 -0800

    [Initial Commit] [Mechanics] - Created 3 working traps - Made timer script that will play a void function - Made Trap Manager that offsets the timing of traps

---
+++ Assets/MyStuff/Scripts/Trap.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trap : MonoBehaviour
+{
+    [SerializeField]
+    Timer MyTimer = new Timer();
+    [SerializeField]
+    float TimeLeft;
+    [SerializeField]
+    Animator MyAnimator;
+    bool StartTrap = false;
+
+    public void Initialize()
+    {
+        StartTrap = true;
+        MyTimer.Initialize(3.0f);
+    }
+
+    void Update()
+    {
+        if (!StartTrap)
+            return;
+
+        MyTimer.TimerAction(ChangeAnimationState);
+    }
+
+    void ChangeAnimationState()
+    {
+        MyAnimator.SetTrigger("Change");
+    }
+}
---
+++ Assets/MyStuff/Scripts/TrapManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TrapManager : MonoBehaviour
+{
+    [SerializeField]
+    Timer TrapStartTimeController = new Timer();
+    [SerializeField]
+    List<Trap> AllTraps = new List<Trap>();
+    int CurrentTrapIndex = 0;
+
+    void Start()
+    {
+        for(int i = 0; i < transform.childCount; ++i)
+        {
+            Trap child = transform.GetChild(i).GetComponent<Trap>();
+            if (child)
+            {
+                AllTraps.Add(child);
+            }
+        }
+        TrapStartTimeController.Initialize(0.5f);
+    }
+    
+    void Update()
+    {
+        if(CurrentTrapIndex < AllTraps.Count)
+        {
+            TrapStartTimeController.TimerAction(StartTrap);
+        }
+    }
+
+    void StartTrap()
+    {
+        AllTraps[CurrentTrapIndex].Initialize();
+        ++CurrentTrapIndex;
+    }
+
+}
